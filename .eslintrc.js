9f380e532a8af1f082d3330d2163f6e50a0218bb
ponchale1 <alfonso.hernandez@astian.org>
2022-08-14 09:38:17 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,9 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+   parser: '@typescript-eslint/parser',
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
-    'prettier/react',
   ],
   parserOptions: {
     ecmaVersion: 2018,
@@ -14,6 +12,8 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/no-var-requires': 'off'
-  }
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
 };
