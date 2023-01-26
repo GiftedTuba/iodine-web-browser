b40a95bc29a43726564b31595c53f45dc7df0160
matthewmyles <matthewwoodard@protonmail.com>
2023-01-26 11:49:12 -0500

    Upadate Themes

--- src/utils/themes.ts
+++ src/utils/themes.ts
@@ -1,11 +1,11 @@
 /* Copyright (c) 2021-2022 SnailDOS */
 
-import { lightTheme, darkTheme, blueTheme, lavTheme } from '~/renderer/constants/themes';
+import { lightTheme, darkTheme, blue_and_orange_theme, purple_and_yellow_theme } from '~/renderer/constants/themes';
 
 export const getTheme = (name: string) => {
   if (name === 'wexond-light') return lightTheme;
   else if (name === 'wexond-dark') return darkTheme;
-  else if (name === 'midori-blue') return blueTheme;
-  else if (name === 'midori-lavender') return lavTheme;
+  else if (name === 'midori-blue') return blue_and_orange_theme;
+  else if (name === 'midori-lavender') return purple_and_yellow_theme;
   return lightTheme;
 };
