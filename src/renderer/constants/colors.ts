17a81678db4ba8597df64f6c2ece612a2ee315bc
matthewmyles <matthewwoodard@protonmail.com>
2023-05-23 15:39:34 -0400

    Cherry

--- src/renderer/constants/colors.ts
+++ src/renderer/constants/colors.ts
@@ -1,66 +1,66 @@
 /* Copyright (c) 2021-2022 SnailDOS */
 
+/* Blue 500 and Blue 300 are functionality colors, the browser depends on them. DO NOT REMOVE, MODIFY OR CHANGE THEM */
+
 export const BLUE_500 = '#21b0f3';
 export const BLUE_300 = '#64B5F6';
 
+/* SHADES OF RED */
+
 export const RED_500 = '#F44336';
 
-export const PINK_500 = '#E91E63';
+export const CHERRY_500 = '#990f24';
 
-export const PURPLE_500 = '#9C27B0';
+export const ROSE_500 = '#E3242B';
 
-export const DEEP_PURPLE_500 = '#673AB7';
+export const JAM_500 = '#60100B'
 
-export const INDIGO_500 = '#3F51B5';
+/* SHADES OF ORANGE */
 
-export const CYAN_500 = '#00BCD4';
+export const ORANGE_500 = '#ED7014';
 
-export const LIGHT_BLUE_500 = '#03A9F4';
+export const TANGERINE_500 = '#FA8128';
 
-export const TEAL_500 = '#009688';
+export const MARIGOLD_500 = '#FCAE1E';
 
-export const GREEN_500 = '#4CAF50';
+export const CIDER_500 = '#B56727';
 
-export const LIGHT_GREEN_500 = '#8BC34A';
+/* SHADES OF YELLOW */
 
-export const LIME_500 = '#CDDC39';
+export const YELLOW_500 = '#FFFD37';
 
-export const YELLOW_500 = '#FFEB3B';
+export const BEIGE_500 = '#EEDC9A';
 
-export const AMBER_500 = '#FFC107';
+export const MACAROON_500 = '#F9E076';
 
-export const ORANGE_500 = '#FF9800';
+export const HAZEL_WOOD_500 = '#C9BB83';
 
-export const purple_and_yellow_500 = '#DED4E8';
+/* SHADES OF GREEN */
 
-export const DEEP_ORANGE_500 = '#FF5722';
+export const GREEN_500 = '#3CB043';
 
-export const BLUE_GRAY_500 = '#607D8B';
+export const CHARTREUSE_500 = '#B0FC38';
 
-export const PEACH_500 = '#EEA47FFF';
+export const JUNIPER_500 = '#3A5311';
 
-export const CHARCOAL_500 = '#101820FF';
+export const SAGE_500 = '#728C69';
 
-export const CORAL_500 = '#F96167';
+/* SHADES OF BLUE */
 
-export const CHERRY_500 = '#990011FF';
+export const BLUE_400 = '#3944BC';
 
-export const BABY_BLUE_500 = '#8AAAE5';
+export const SLATE_500 = '#757C88';
 
-export const HOT_PINK_500 = '#FF69B4';
+export const SKY_500 = '#63C5DA';
 
-export const SKY_BLUE_500 = '#89ABE3FF';
+export const NAVY_500 = '#0A1172';
 
-export const SAGE_500 = '#CBD18F';
+/* SHADES OF PURPLE */
 
-export const APPLE_500 = '#783937FF';
+export const PURPLE_500 = '#A32CC4';
 
-export const FOREST_GREEN_500 = '#2C5F2D';
+export const MAUVE_500 = '#7A4988';
 
-export const ISLAND_GREEN_500 = '#2BAE66FF';
+export const VIOLET_500 = '#710193';
 
-export const BEIGE_500 = '#DDC3A5';
-
-export const TAN_500 = '#E0A96D';
-
-export const MAROON_500 = '#7A2048';
+export const BOYSENBERRY_500 = '#630436';
